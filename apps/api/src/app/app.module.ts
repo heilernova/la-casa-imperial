c43d42a8d95e3ef47611ec25d2629648d7e5c6f1
Heiler Nova <heilernova@outlook.com>
2024-11-29 13:18:10 -0500

    feat(api): crear módulo models
    Se crea el módulo  para manejar todos los servicios globales que
    tengan interacción con los datos.

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
+import { ModelsModule } from './models/models.module';
 
 @Module({
   imports: [
-    CommonModule
+    CommonModule,
+    ModelsModule
   ],
   controllers: [AppController],
   providers: [AppService],
