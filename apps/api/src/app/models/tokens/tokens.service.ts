ba4d2734684d9c863aa9bb45fbad622dc2c46306
Heiler Nova <heilernova@outlook.com>
2024-11-30 09:06:15 -0500

    feat(types): agregar tipo 'Sex'
    Se agrega un nuevo tipo  que puede tener uno de dos valores:  para masculino y  para femenino.
    Este tipo se utiliza para estandarizar la representación del sexo en el sistema.

---
+++ apps/api/src/app/models/tokens/tokens.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import { DbConnectionService } from '../../common/db-connection';
+
+interface IToken {
+    id: string
+    createAt: Date
+    userId: string
+    exp: Date | null
+    ip: string
+    platform: string
+    device: string
+}
+
+@Injectable()
+export class TokensService {
+    constructor(private readonly _db: DbConnectionService){}
+
+    public async get(id: string, userId?: string): Promise<IToken> { 
+        let sql = `SELECT id, create_at as "createAt", user_id as "userId", exp, ip, platform, device FROM users_tokens WHERE id = $1`;
+        const params: string[] = [id];
+        if (userId){
+            sql += " and user_id = $2";
+            params.push(userId);
+        }
+        return (await this._db.query(sql, params)).rows[0];
+    }
+
+    public async getAll(filters?: { userId?: string }): Promise<IToken[]> {
+        const conditions: string[] = [];
+        let sql = `id, create_at as "createAt", user_id as "userId", exp, ip, platform, device FROM users_tokens`;
+        let params: string[] | undefined = [];
+        if (filters?.userId){
+            conditions.push(`user_id = $${params.push(filters.userId)}`);
+        }
+
+        if (conditions.length > 0)  {
+            sql += " where " + conditions.join(" and ");
+        } else {
+            params = undefined;
+        }
+        return (await this._db.query(sql, params)).rows;
+    }
+
+    public async delete(id: string | string[]): Promise<void> {
+        if (Array.isArray(id)){
+            await this._db.query("DELETE FORM users_tokens WHERE id = ANY($1)", [id]);
+        } else {
+            await this._db.query("DELETE FORM users_tokens WHERE id = $1", [id]);
+        }
+    }
+}
