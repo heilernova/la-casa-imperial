49df3a0d97e2758db4657ca3d089d2bbdf4cf845
Heiler Nova <heilernova@outlook.com>
2024-12-02 12:10:54 -0500

    chore(api): exportar servicios y interfaces de inventario
    - Se exporta `CategoriesService` desde `./categories/categories.service`.
    - Se exporta `item-db-values.interfaces` desde `./items/item-db-values.interfaces`.
    - Se exporta `ItemsService` desde `./items/items.service`.
    - Se exporta `sql-query-products` desde `./items/sql-query-products`.
    
    Estas exportaciones facilitan el acceso a estos servicios y interfaces desde otros módulos del proyecto.

---
+++ apps/api/src/app/models/inventory/index.ts
@@ -0,0 +1,4 @@
+export * from './categories/categories.service';
+export * from './items/item-db-values.interfaces';
+export * from './items/items.service';
+export * from './items/sql-query-products';
