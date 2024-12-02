2ec745e924780e522bb695437ce163ae40bec407
Heiler Nova <heilernova@outlook.com>
2024-12-02 17:53:45 -0500

    chore(api): agregar ItemPipe

--- apps/api/src/app/models/inventory/index.ts
+++ apps/api/src/app/models/inventory/index.ts
@@ -2,3 +2,4 @@ export * from './categories/categories.service';
 export * from './items/item-db-values.interfaces';
 export * from './items/items.service';
 export * from './items/sql-query-products';
+export * from './items/item.pipe';
