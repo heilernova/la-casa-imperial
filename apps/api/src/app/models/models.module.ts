fc567a3302d3eb78903ec03d3c88e895b82f742f
Heiler Nova <heilernova@outlook.com>
2024-12-02 10:37:40 -0500

    chore(api): agregar servicios con servicios globales del al módulo models
    Se agregan servicios de datos para
    - Se importan `UsersService`, `CategoriesService`, `ItemsService` y `TokensService` de sus respectivos módulos.
    - Se definen los servicios en la constante.
    - Se agregan al modulo para su exportación.

--- apps/api/src/app/models/models.module.ts
+++ apps/api/src/app/models/models.module.ts
@@ -1,5 +1,14 @@
 import { Global, Module } from '@nestjs/common';
+import { UsersService } from './users/users.service';
+import { CategoriesService } from './inventory/categories/categories.service';
+import { ItemsService } from './inventory/items/items.service';
+import { TokensService } from './tokens/tokens.service';
+
+const services = [UsersService, CategoriesService, ItemsService, TokensService];
 
 @Global()
-@Module({})
+@Module({
+  providers: services,
+  exports: [UsersService, CategoriesService, ItemsService, TokensService]
+})
 export class ModelsModule {}
