f4572b16aaa71385457402a811b5c753c0e6e325
Heiler Nova <heilernova@outlook.com>
2024-12-02 14:43:51 -0500

    feat(api): crear modulo de los controladores
    Se crea el módulo `ControllersModule` para agregar los controladores de la API
    - Se crea el módulo `ControllersModule`.
    - Esto centraliza y organiza la gestión de controladores en un solo módulo.

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
 import { ModelsModule } from './models/models.module';
+import { ControllersModule } from './controllers/controllers.module';
 
 @Module({
   imports: [
     CommonModule,
-    ModelsModule
+    ModelsModule,
+    ControllersModule
   ],
   controllers: [AppController],
   providers: [AppService],
