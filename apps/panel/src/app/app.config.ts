a740aa7367028aad115afc46c7a7a767f1ed436c
Heiler Nova <heilernova@outlook.com>
2024-12-03 16:32:05 -0500

    feat(panel): agregar NG-ZORRO para componentes de interfaz de usuario
    - Se instala NG-ZORRO y sus dependencias.
    - Se añade NG-ZORRO a los módulos del proyecto.
    - Se configuran los estilos globales para utilizar NG-ZORRO.
    
    Esta adición proporciona un conjunto robusto de componentes de UI basados en Ant Design, mejorando la apariencia y funcionalidad del proyecto.

--- apps/panel/src/app/app.config.ts
+++ apps/panel/src/app/app.config.ts
@@ -1,13 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { apiInterceptor } from './config/api.interceptor';
+import { es_ES, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import es from '@angular/common/locales/es';
+import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+registerLocaleData(es);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
-    provideHttpClient(withInterceptors([apiInterceptor]))
+    provideHttpClient(withInterceptors([apiInterceptor])),
+    provideNzI18n(es_ES),
+    importProvidersFrom(FormsModule),
+    provideAnimationsAsync(),
+    { provide: LOCALE_ID, useValue: "es" }
   ],
 };
