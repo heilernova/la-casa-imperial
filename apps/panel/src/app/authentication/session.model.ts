3473c071732f0f09edf1ba6af5b33fb0eb0c6a7a
Heiler Nova <heilernova@outlook.com>
2024-12-04 16:33:04 -0500

    chore(panel): agregar nueva funcionalidades a la clase Session
    - Se le agrega la propiedad HttClient para conectar con la API.
    - Se le agrega el método `update` para actualizar la información del usuario.
    - Se le agrega el método `getInfo` para obtener la información del usuario.

--- apps/panel/src/app/authentication/session.model.ts
+++ apps/panel/src/app/authentication/session.model.ts
@@ -1,5 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { ApiSession } from '@la-casa-imperial/schemas/auth';
 import { UserRole } from '@la-casa-imperial/schemas/users';
+import { ApiProfile, ApiProfileUpdate  } from '@la-casa-imperial/schemas/profile';
+import { capitalize } from '@la-casa-imperial/core';
+
 export class Session {
     private readonly pin: string | null;
     public readonly token: string;
@@ -9,7 +13,7 @@ export class Session {
     public readonly username: string;
     public readonly permissions: string[];
 
-    constructor(data: ApiSession){
+    constructor(data: ApiSession, private readonly _http: HttpClient){
         this.token = data.token;
         this.name = data.name;
         this.lastName = data.lastName;
@@ -28,5 +32,37 @@ export class Session {
             Array.isArray(this.permissions)
         ].every(Boolean);
     }
-      
+
+    getInfo(): Promise<ApiProfile> {
+        return new Promise((resolve, reject) => {
+
+            this._http.get<ApiProfile>("profile").subscribe({
+                next: res => {
+                    resolve(res);
+                },
+                error: err => reject(err)
+            })
+        });
+    }
+    
+    update(data: ApiProfileUpdate): Promise<void> {
+        return new Promise((resolve, reject) => {
+            this._http.put<void>("profile", data).subscribe({
+                next: () => {
+                    if (data.email) data.email = data.email.toLowerCase();
+                    if (data.name) data.name = capitalize(data.name);
+                    if (data.lastName) data.lastName = capitalize(data.lastName);
+                    
+                    const properties = Object.fromEntries(
+                        Object.entries(data).map(([key, value]) => [key, { value }])
+                    );
+                    
+                    Object.defineProperties(this, properties)
+
+                    resolve();
+                },
+                error: err => reject(err)
+            })
+        })
+    }
 }
