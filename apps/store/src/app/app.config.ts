618730baf5a4acf0de2e47f68ca3553fb49956c7
Heiler Nova <heilernova@outlook.com>
2024-12-11 14:08:36 -0500

    chore(store): configurar httpClient.

--- apps/store/src/app/app.config.ts
+++ apps/store/src/app/app.config.ts
@@ -2,10 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
+import { apiInterceptor } from './config/api.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(),
+    provideHttpClient(withFetch(), withInterceptors([apiInterceptor])),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
   ],
